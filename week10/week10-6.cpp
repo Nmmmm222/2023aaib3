d894df605d3dd9f71e01fb525764704e48b7dddb
Nmmmm222 <alisawu0606@gmail.com>
2024-11-13 11:28:14 +0800

    week10

---
+++ week10/week10-6.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        //全部乘起來的正負號
+        int total=1;
+        for(int i=0;i<nums.size();i++){
+            //total*=nums[i]; ///數字不能乘太大
+            if(nums[i]>0) total*=1;
+            if(nums[i]<0) total*=-1;
+            if(nums[i]==0) total*=0;
+
+        }
+        if(total>0) return 1;
+        if(total<0) return -1;
+        return 0;
+    }
+};
